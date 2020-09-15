9016f72d89106e865e1639423e13d21fd279e337
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-09-15 11:41:06 +0100

    User input text, Click listener function

--- app/src/main/java/com/example/aboutmeapp/MainActivity.kt
+++ app/src/main/java/com/example/aboutmeapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.aboutmeapp
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.INVISIBLE
+        view.visibility = View.INVISIBLE
+
+        nicknameTextView.visibility = View.VISIBLE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
